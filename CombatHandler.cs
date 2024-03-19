3bc1d4a9de18b5a71a5aa9e53e6bc424a88ee70b
Simon-NTI <simon.hall@elev.ga.nti.gse>
2024-03-19 14:54:40 +0100

    Made basic logic for CombatHandler

--- CombatHandler.cs
+++ CombatHandler.cs
@@ -1,4 +1,61 @@
+using System.Diagnostics;
+
 class CombatHandler
 {
-    
+    float deltaTime, timeUntilNextUpdate, targetUpdateRate;
+    bool stop = false;
+    int _targetUpdateRate = 10;
+    int framesElapsed = 0;
+
+    public void Start()
+    {
+        //mainThread = new Thread(Update);
+        targetUpdateRate = 1f / _targetUpdateRate * 1000;
+        timeUntilNextUpdate = targetUpdateRate;
+        Update();
+    }
+    private void Update()
+    {
+        Stopwatch debugTimer = new();
+        debugTimer.Start();
+        Stopwatch stopwatch = new();
+        while(true)
+        {
+            stopwatch.Start();
+
+            // Primary logic
+            if(!stop)
+            {
+                NextStep();
+                framesElapsed += 1;
+                Console.WriteLine("This is frame nr: " + framesElapsed);
+            }
+
+            stopwatch.Stop();
+            deltaTime = stopwatch.ElapsedMilliseconds;
+            timeUntilNextUpdate = targetUpdateRate - stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            Console.WriteLine("Until Next: " + timeUntilNextUpdate);
+            Console.WriteLine("Deltatime: " + deltaTime);
+
+            if(timeUntilNextUpdate >= 0)
+            {
+                //await Task.Delay((int)timeUntilNextUpdate);
+                Thread.Sleep((int)timeUntilNextUpdate);
+            }
+            if(framesElapsed > 200)
+            {
+                debugTimer.Stop();
+                Console.WriteLine("Total time elapsed: " + debugTimer.ElapsedMilliseconds);
+                Console.WriteLine("Total frames: " + framesElapsed);
+                break;
+            }
+        }
+    }
+
+    private void NextStep()
+    {
+
+
+    }
 }
--- Enemy.cs
+++ Enemy.cs
@@ -1,4 +1,14 @@
 class Enemy
 {
+    Item[] lootTable; // The sum of all items in the loot table must equal 100
+                      // The odds in the table are expressed in %
+    int health, damage;
+    float attackSpeed, attackCooldown;
+
+    public void PerformActions()
+    {
+
+    }
+
     
 }
